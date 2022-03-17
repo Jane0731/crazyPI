bb8ea09c7061616a4acd96b3c3cc1a715d92f86d
unknown <garyopen1876@gmail.com>
2022-03-17 13:57:18 +0800

    sec update

--- ht/views.py
+++ ht/views.py
@@ -5,10 +5,10 @@ import datetime
 def home(request):
     last_one = temperature_humidity.objects.last()
     last= temperature_humidity.objects.all().order_by('-id')[:6]
-    last_six = last[1]
-    last_five = last[2]
+    print(last[1].humidity)
+    last_six = last[5]
+    last_five = last[4]
     last_four = last[3]
-    last_three = last[4]
-    last_two =last[5]
-
-    return render(request,'crazypi.html', locals())
+    last_three = last[2]
+    last_two =last[1]
+    return render(request,'page.html', locals())
