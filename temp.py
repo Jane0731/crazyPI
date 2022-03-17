9fe3903e6aada9d92195ec157c2d491b287a142d
jane0731 <jane991680420@outlook.com>
2022-03-17 03:34:34 +0000

    MQTT setup

--- temp.py
+++ temp.py
@@ -1,11 +1,22 @@
+#from sqlite3 import Cursor
 import Adafruit_DHT 
+import paho.mqtt.client as mqtt
+import random
+import json  
+import datetime 
+import time
 
-#DHT_SENSOR = Adafruit_DHT.DHT22 
-#DHT_PIN = 4  
-
+client = mqtt.Client()
+client.username_pw_set("jane","zxc123asd")
+client.connect("192.168.168.77", 1883, 60)
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11,4)
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        payload = {'Temperature' : temperature , 'humidity' : humidity}
+        print (json.dumps(payload))
+        #要發布的主題和內容
+        client.publish("Try/MQTT/Temperature_humidity", json.dumps(payload))
+        time.sleep(5)
     else:
         print("Failed to retrieve data from HDT22 sensor")
