b66d824d1c812b65506f6ee7d18375e568339b68
jane0731 <jane991680420@outlook.com>
2022-03-18 02:06:25 +0000

    Add Time

--- temp.py
+++ temp.py
@@ -1,19 +1,21 @@
 #from sqlite3 import Cursor
 import Adafruit_DHT 
 import paho.mqtt.client as mqtt
-import random
 import json  
-import datetime 
 import time
+from datetime import datetime, timezone, timedelta
+from pytz import timezone
 
 client = mqtt.Client()
 client.username_pw_set("jane","zxc123asd")
 client.connect("192.168.168.77", 1883, 60)
+tz = timezone('Asia/Taipei')
+now = datetime.now(tz)
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11,4)
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        payload = {'Temperature' : temperature , 'Humidity' : humidity}
+        payload = {'Temperature' : temperature , 'Humidity' : humidity,'Time':str(now.hour)+":"+str(now.minute)}
         print (json.dumps(payload))
         #要發布的主題和內容
         client.publish("Try/MQTT/Temperature_humidity", json.dumps(payload))
